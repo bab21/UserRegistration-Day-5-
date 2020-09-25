b5fbffaecd23e37265eed20cf3d1ad32969273ea
bab21 <2016061@iiitdmj.ac.in>
2020-09-25 22:05:11 +0530

    validating first name

---
+++ UserRegistration.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;
+import java.util.Scanner;
+public class UserRegistration{
+	
+	public static void main(String[] args){
+	   Scanner s=new Scanner(System.in);
+		
+       String pattern_first_name="[A-Z][a-z]{2,}";
+	   Pattern p = Pattern.compile(pattern_first_name); 
+	   
+	   String first_name=s.nextLine();
+	   Matcher m1=p.matcher(first_name);
+	   System.out.println("given word matches(true/false):"+m1.matches());
+	}
+}
